c941f6d6b6c426bd52b5401c125c59e00307c063
Swatikumari-github <106343290+Swatikumari-github@users.noreply.github.com>
2025-03-04 12:10:08 +0530

    add gitignore

--- BusinessLayer/obj/Debug/net6.0/BusinessLayer.AssemblyInfo.cs
+++ BusinessLayer/obj/Debug/net6.0/BusinessLayer.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("BusinessLayer")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+00afe1b02692652983e39e595a4e1913d43ae34b")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+a783e86d4f0bfff614d712571afa377eb70cb050")]
 [assembly: System.Reflection.AssemblyProductAttribute("BusinessLayer")]
 [assembly: System.Reflection.AssemblyTitleAttribute("BusinessLayer")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
